acf763f2d6254298c949c48553f27791bdc1f9a0
Colin O'Brien <static.realtiy@gmail.com>
2011-01-24 14:37:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fritter::Application.config.secret_token = '3a3c0583db28068a28fe8d1cad78e6c9d92186368ff28305475b6c2c0669802356b4f3ded30181ade7b8ec3a70644006f0ad525aa1f1e7beb31d37961dc4f6d3'
